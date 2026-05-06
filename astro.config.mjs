45d6a3997d0e0a95330b05c20ff4a8de6618ac23
Freya Bia Site <freya-bia-site@users.noreply.github.com>
2026-05-06 01:06:34 +0000

    Completa SEO tecnico e paginas legais

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lp-bia-munaretto.vercel.app',
+  site: 'https://www.biamunareto.com.br',
   integrations: [],
   build: {
     inlineStylesheets: 'auto',
